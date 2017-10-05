28305d18256726cd1541d772e2392a4162b881bd
Phaninder <phaninder2008@gmail.com>
2017-10-06 01:24:50 +0530

    Shader program

--- LearningOpenGL/OpenGLTest/OpenGLTest/TriangleExercise.cpp
+++ LearningOpenGL/OpenGLTest/OpenGLTest/TriangleExercise.cpp
@@ -9,12 +9,41 @@ using namespace std;
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
+//const char *vertexShaderSource = "#version 410 core\n"
+//"layout (location=0) in vec3 aPos;\n"
+//"void main()\n"
+//"{\n"
+//"	gl_Position=vec4(aPos.x,aPos.y,aPos.x,1.0f);\n"
+//"}\0";
+//
+//const char *fragShaderSource = "#version 410 core\n"
+//"out vec4 FragColor;\n"
+//"void main()\n"
+//"{\n"
+//"	FragColor=vec4(1.0f,0.25f,0.35f,1.0f);\n"
+//"}\n\0";
+
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char *fragShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+
 int DrawTriangle()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_VERSION_MINOR, 3);
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
+	glfwInit();
 	GLFWwindow *window = glfwCreateWindow(800, 600, "Triangle", nullptr, nullptr);
 
 	if (window == nullptr)
@@ -31,6 +60,55 @@ int DrawTriangle()
 		cout << "Failed to initialise GLAD" << endl;
 		return -1;
 	}
+	
+	//Compile shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+		cout << "Error compiling vertex shader:" << infoLog << endl;
+	}
+
+	//Compile frag shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(fragmentShader, 1, &fragShaderSource, nullptr);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+		cout << "Error compiling fragment shader" << infoLog << endl;
+	}
+
+	//Make shader program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+		cout << "Error linking shader program:" << infoLog << endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	// Create vbo and vao
 	GLfloat vertices[] = {
@@ -92,7 +170,7 @@ int DrawTriangle()
 		glClearColor(0.2f, 0.5f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-
+		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		//glDrawArrays(GL_TRIANGLES, 0, 6);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -105,6 +183,7 @@ int DrawTriangle()
 	glDeleteBuffers(1, &vbo);
 	glDeleteBuffers(1, &EBO);
 
+	glfwTerminate();
 //	glDeleteVertexArrays(1, &VAO1);
 	//glDeleteBuffers(1, &vbo1);
 	//glDeleteBuffers(1, &vbo1);
