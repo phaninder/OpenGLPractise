5b215331a9e5a65d0528115e2b8c8c898760ad4e
Phaninder <phaninder2008@gmail.com>
2017-10-09 22:08:02 +0530

    Added seperate files for shaders

--- LearningOpenGL/OpenGLTest/OpenGLTest/TriangleExercise.cpp
+++ LearningOpenGL/OpenGLTest/OpenGLTest/TriangleExercise.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "Shader.h"
 
 using namespace std;
 
@@ -23,23 +24,23 @@ void processInput(GLFWwindow *window);
 //"	FragColor=vec4(1.0f,0.25f,0.35f,1.0f);\n"
 //"}\n\0";
 
-const char *vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"out vec4 vertexColor;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"	vertexColor = vec4(aColor,1.0f);\n"
-"}\0";
-const char *fragShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec4 vertexColor;\n"
-"uniform vec4 ourColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor =vertexColor;\n"
-"}\n\0";
+//const char *vertexShaderSource = "#version 330 core\n"
+//"layout (location = 0) in vec3 aPos;\n"
+//"layout (location = 1) in vec3 aColor;\n"
+//"out vec4 vertexColor;\n"
+//"void main()\n"
+//"{\n"
+//"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//"	vertexColor = vec4(aColor,1.0f);\n"
+//"}\0";
+//const char *fragShaderSource = "#version 330 core\n"
+//"out vec4 FragColor;\n"
+//"in vec4 vertexColor;\n"
+//"uniform vec4 ourColor;\n"
+//"void main()\n"
+//"{\n"
+//"   FragColor =vertexColor;\n"
+//"}\n\0";
 
 
 int DrawTriangle()
@@ -66,67 +67,8 @@ int DrawTriangle()
 		return -1;
 	}
 	
-	//Compile shader
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-	glCompileShader(vertexShader);
-
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-		cout << "Error compiling vertex shader:" << infoLog << endl;
-	}
-
-	//Compile frag shader
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	glShaderSource(fragmentShader, 1, &fragShaderSource, nullptr);
-	glCompileShader(fragmentShader);
-
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-		cout << "Error compiling fragment shader" << infoLog << endl;
-	}
-
-	//Make shader program
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success)
-	{
-		glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-		cout << "Error linking shader program:" << infoLog << endl;
-	}
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	// Create vbo and vao
-	//GLfloat vertices[] = {
-	//	// first triangle
-	//	-0.5f,0.5f,0.0f,
-	//	-0.75f,-0.5f,0.0f,
-	//	-0.25f,-0.5f,0.0f,
-	//	// second triangle
-	//	0.5f,0.5f,0.0f,
-	//	0.75f,-0.5f,0.0f,
-	//	0.25f,-0.5f,0.0f
-	//};
-
+	Shader ourShader("vertex3.txt", "fragment3.txt");
+	
 	GLfloat vertices[] = {
 		// positions         // colors
 		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
@@ -138,13 +80,6 @@ int DrawTriangle()
 		0,1,2,
 		//3,4,5
 	};
-
-/*
-	GLfloat vertices2[] = {
-		0.5f,0.5f,0.0f,
-		0.75f,-0.5f,0.0f,
-		0.25f,-0.5f,0.0f
-	};*/
 
 	//Create buffer and store data
 	unsigned int vbo, VAO,EBO;
@@ -167,19 +102,7 @@ int DrawTriangle()
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	/*unsigned int vbo1, VAO1;
-	glGenVertexArrays(1, &VAO1);
-	glBindVertexArray(VAO1);
-	glGenBuffers(1, &vbo1);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
 
-	//Create vertex array obj and store vbo
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	*/
-
-	int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
@@ -189,8 +112,12 @@ int DrawTriangle()
 
 		float timeValue = glfwGetTime();
 		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		glUseProgram(shaderProgram);
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		
+		//glUseProgram(shaderProgram);
+		ourShader.use();
+		//int vertexColorLocation =glGetUniformLocation(shaderProgram, "ourColor");
+		ourShader.setVector3("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
+		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		glBindVertexArray(VAO);
 		//glDrawArrays(GL_TRIANGLES, 0, 6);
 		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
